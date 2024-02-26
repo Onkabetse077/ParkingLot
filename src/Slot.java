4aaa08e54d2779d6f9d20138946fe9db8d259ad4
Onkabetse <113599354+Onkabetse077@users.noreply.github.com>
2024-02-26 19:53:41 +0200

    Simple Parking Lot Management System

--- src/Slot.java
+++ src/Slot.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Slot {
+public class Slot {
+    private String type;
+    private  Vehicle vehicle;
+    private String ticketID;
+
+    public Slot(){
+        type = "Sedan";
+        vehicle = getVehicle();
+        ticketID = "001";
+    }
+    public Slot(String type,Vehicle vehicle,String ticketID){
+        this.type = type;
+        this.vehicle = vehicle;
+        this.ticketID = ticketID;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public String getTicketID() {
+        return ticketID;
+    }
+
+    public void setTicketID(String ticketID) {
+        this.ticketID = ticketID;
+    }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Vehicle {
+public class Vehicle {
+    private String type;
+    private String registration;
+    private String colour;
+
+    public Vehicle(){
+        type = "Sedan";
+        registration = "XX 01 OG L";
+        colour = "Blue";
+    }
+    public Vehicle(String type,String registration,String colour){
+        this.type = type;
+        this.registration = registration;
+        this.colour = colour;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(String registration) {
+        this.registration = registration;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
 }
